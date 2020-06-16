5f60c920742a6e6bad14b71b9c5ed449aa332cd4
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-06-16 19:21:28 +0200

    adding view for create task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,8 +5,26 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+    authorize @task
   end
 
   def create
+    @job = Job.find(49)
+    @task = Task.new(task_params)
+    @task.job = @job
+    authorize @task
+    if @task.save!
+      redirect_to job_path(49)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
 end
