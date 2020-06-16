8def0380b79f0d41bd7f1e6985d402b5b3d42b49
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-06-16 14:45:38 +0200

    creating jobs view

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,17 @@
 class JobsController < ApplicationController
+  # skip_before_action :authenticate_user!, only: [:index_constructor]
+
+  def index_constructor
+    @jobs = policy_scope(Jobs).where(user_id: current_user)
+  end
+
+  def show_constructor
+  end
+
+  def index
+  end
+
+  def show
+  end
+
 end
