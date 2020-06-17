c4eed032ffe724bc1d9c84c72e0b0a137befe9e4
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-06-17 11:57:04 +0200

    adding documents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,11 +1,20 @@
 class DocumentsController < ApplicationController
   def index
+    if current_user.constructor?
+      @documents = policy_scope(Document).where(user_id: params[:user_id])
+    else
+      @documents = policy_scope(Document).where(user: current_user)
+    end
   end
 
   def show
+    @document = Document.find(params[:id])
+    authorize @document
   end
 
   def new
+    @document = Document.new
+    authorize @document
   end
 
   def create
