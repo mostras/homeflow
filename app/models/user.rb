b123b4ae0dd3d5d71010616c72b36bb704258db0
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-06-19 11:06:33 +0200

    adding avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :documents
   has_many :jobs
   has_many :tasks, through: :jobs
+  has_one_attached :photo
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
