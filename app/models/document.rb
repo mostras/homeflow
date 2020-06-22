55828bd07e74b4b6f9158cee32515d43cb553e18
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-06-22 14:16:36 +0200

    Ajoute la fonctionalite de chercher des documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,6 +2,10 @@ class Document < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
 
+  include PgSearch::Model
+
+  pg_search_scope :search, against: [:title, :category]
+
   DOC_TYPE = %w[banque plan administratif]
 
   validates :title, presence: true
