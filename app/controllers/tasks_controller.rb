a0a32bda54d3f053ad989b450257b2c25fb32daa
Romain <romainmostras@icloud.com>
2020-06-22 16:49:52 +0200

    ajout de la fonction pour envoyer des emails

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,6 +19,7 @@ class TasksController < ApplicationController
     if @task.save
       @job.completed = false
       @job.save
+      send_notification_task_email
       redirect_to job_path(@job)
     else
       render :new
@@ -26,6 +27,10 @@ class TasksController < ApplicationController
   end
 
   private
+
+  def send_notification_task_email
+    TaskMailer.with(client: @task.job.user, task: @task).notification.deliver_later
+  end
 
   def task_params
     params.require(:task).permit(:title, :content, photos: [])
