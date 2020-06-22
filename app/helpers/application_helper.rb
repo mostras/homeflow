c774cbafb83d81813ab56d64464e5bd804db18e3
Romain <romainmostras@icloud.com>
2020-06-22 11:48:07 +0200

    changement de couleur pour la tab bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
 
-end
+  def active_class_for(tab)
+    string = "#{params[:controller]}-#{params[:action]}-#{tab}"
 
+    case string
+    when 'users-show-jobs' then 'active'
+    when 'jobs-index-jobs' then 'active'
+    when 'documents-index-documents' then 'active'
+    when 'users-details-details' then 'active'
+    end
+  end
+end
