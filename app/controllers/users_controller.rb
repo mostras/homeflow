8f14c834e09b30ea2511f4b3a68714a47337f542
Romain <romainmostras@icloud.com>
2020-06-23 12:35:35 +0200

    creation des deux autres emails pour mettre/enlever le chantier en pause et pour terminer un job

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,11 +14,15 @@ class UsersController < ApplicationController
 
   def stop_work
     @client.update!(in_progress: false)
+    authorize @client
+    send_work_paused_email
     redirect_back(fallback_location: users_path)
   end
 
   def resume_work
     @client.update!(in_progress: true)
+    authorize @client
+    send_work_paused_email
     redirect_back(fallback_location: users_path)
   end
 
@@ -31,5 +35,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :first_name, :last_name, :address_street, :address_zip, :address_city, :phone_number)
+  end
+
+  def send_work_paused_email
+    TaskMailer.with(client: @client).work_paused.deliver_later
   end
 end
--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,14 +1,21 @@
 class TaskMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.task_mailer.new.subject
-  #
   def notification
     @client = params[:client]
     @task = params[:task]
 
     mail to: @client.email, subject: "Votre chantier avance !"
+  end
+
+  def work_paused
+    @client = params[:client]
+
+    mail to: @client.email, subject: @client.in_progress? ? "Votre chantier reprend !" : "Nous revenons très vite !"
+  end
+
+  def job_done
+    @client = params[:client]
+    @job = params[:job]
+
+    mail to: @client.email, subject: "L'étape #{@job.name} est terminée !"
   end
 end
